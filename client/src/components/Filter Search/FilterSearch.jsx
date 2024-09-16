591bc5d4ec1d921f8a93442cfb67400dbd46b6fb
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-09-17 00:49:02 +0300

    adding the reset filter functionality to filter form

--- client/src/components/Filter Search/FilterSearch.jsx
+++ client/src/components/Filter Search/FilterSearch.jsx
@@ -7,8 +7,8 @@ function FilterSearch({onClose}) {
     const [filters, setFilters] = useState({
         bedrooms: "",
         bathrooms: "",
-        minPrice: null,
-        maxPrice: null,
+        minPrice: "",
+        maxPrice: "",
         propertyType: "show all",
         furnishOptions: "show all",
         sortBy: "Anytime"
@@ -18,7 +18,21 @@ function FilterSearch({onClose}) {
         const name = event.target.name;
         const value = event.target.value;
         setFilters({...filters, [name]: value});
-        console.log(name, value)
+        console.log(filters)
+    }
+
+    function resetFilters() {
+        setFilters({
+            bedrooms: "",
+            bathrooms: "",
+            minPrice: "",
+            maxPrice: "",
+            propertyType: "show all",
+            furnishOptions: "show all",
+            sortBy: "Anytime",
+        })
+
+        console.log(filters)
     }
 
     return (
@@ -32,7 +46,7 @@ function FilterSearch({onClose}) {
                 <div className={styles.smallChooseDiv}>
                     <div className={styles.smallerChoosediv}>
                         <h4>Bedrooms</h4>
-                        <select className={styles.smallSelect} name="bedrooms" id="bedrooms" onChange={selectChagne}>
+                        <select className={styles.smallSelect} name="bedrooms" id="bedrooms" onChange={selectChagne} value={filters.bedrooms}>
                             <option value="">Any</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -43,7 +57,7 @@ function FilterSearch({onClose}) {
                     </div>
                     <div className={styles.smallerChoosediv}>
                         <h4>Bathrooms</h4>
-                        <select className={styles.smallSelect} name="bathrooms" id="bathrooms" onChange={selectChagne}>
+                        <select className={styles.smallSelect} name="bathrooms" id="bathrooms" onChange={selectChagne} value={filters.bathrooms}>
                             <option value="">Any</option>
                             <option value="1">1</option>
                             <option value="2">2</option>
@@ -55,17 +69,17 @@ function FilterSearch({onClose}) {
                 <div className={styles.smallChooseDiv}>
                     <div className={styles.smallerChoosediv}>
                         <h4>Min Price</h4>
-                        <input className={styles.smallSelect} type="number" placeholder='No Min' name='minPrice' onChange={selectChagne}/>
+                        <input className={styles.smallSelect} type="number" placeholder='No Min' name='minPrice' onChange={selectChagne} value={filters.minPrice}/>
                     </div>
                     <div className={styles.smallerChoosediv}>
                         <h4>Max Price</h4>
-                        <input className={styles.smallSelect} type="number" placeholder='No Max' name='maxPrice' onChange={selectChagne}/>
+                        <input className={styles.smallSelect} type="number" placeholder='No Max' name='maxPrice' onChange={selectChagne} value={filters.maxPrice}/>
                     </div>
                 </div>
 
                 <div className={styles.bigChooseDiv}>
                     <h4>Property Type</h4>
-                    <select className={styles.bigSelect} name="propertyType" id="propertyType"  onChange={selectChagne}>
+                    <select className={styles.bigSelect} name="propertyType" id="propertyType"  onChange={selectChagne} value={filters.propertyType}>
                         <option value="Show all">Show all</option>
                         <option value="Saona">Saona</option>
                         <option value="Jacozy">Jacozy</option>
@@ -74,7 +88,7 @@ function FilterSearch({onClose}) {
 
                 <div className={styles.bigChooseDiv}>
                     <h4>Furnished options</h4>
-                    <select className={styles.bigSelect} name="furnishOptions" id="furnishOptions"  onChange={selectChagne}>
+                    <select className={styles.bigSelect} name="furnishOptions" id="furnishOptions" onChange={selectChagne} value={filters.furnishOptions}>
                         <option value="Show all">Show all</option>
                         <option value="Fully-furnished">Fully-furnished</option>
                         <option value="Semi-furnished">Semi-furnished</option>
@@ -84,35 +98,35 @@ function FilterSearch({onClose}) {
 
                 <div className={styles.sortMainDiv}>
                     <h4>Sort order</h4>
-                    <form action="" onChange={selectChagne} name='sortBy'>
+                    <form action="" onChange={selectChagne} name='sortBy' value={filters.sortBy}>
                         <div className={styles.sortDiv}>
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option1" name="sortBy" value="Anytime" />
+                                <input type="radio" id="option1" name="sortBy" value="Anytime" checked={filters.sortBy === "Anytime"}/>
                                 <label htmlFor='option1'>Anytime</label>
                             </div>
 
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option2" name="sortBy" value="24hours" />
+                                <input type="radio" id="option2" name="sortBy" value="24hours" checked={filters.sortBy === "24hours"}/>
                                 <label htmlFor='option2'>Last 24 hours</label>
                             </div>
 
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option3" name="sortBy" value="3days" />
+                                <input type="radio" id="option3" name="sortBy" value="3days" checked={filters.sortBy === "3days"}/>
                                 <label htmlFor='option3'>Last 3 days</label>
                             </div>
 
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option4" name="sortBy" value="7days" />
+                                <input type="radio" id="option4" name="sortBy" value="7days" checked={filters.sortBy === "7days"}/>
                                 <label htmlFor='option1'>Last 7 days</label>
                             </div>
 
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option5" name="sortBy" value="14days" />
+                                <input type="radio" id="option5" name="sortBy" value="14days" checked={filters.sortBy === "14days"}/>
                                 <label htmlFor='option2'>Last 14 days</label>
                             </div>
 
                             <div className={styles.radioDiv}>
-                                <input type="radio" id="option6" name="sortBy" value="30days" />
+                                <input type="radio" id="option6" name="sortBy" value="30days" checked={filters.sortBy === "30days"}/>
                                 <label htmlFor='option3'>Last 30 days</label>
                             </div>
                         </div>
@@ -120,7 +134,7 @@ function FilterSearch({onClose}) {
                 </div>
 
                 <div className={styles.buttonsDiv}>
-                    <button className={styles.resetButton}>Reset filters</button>
+                    <button className={styles.resetButton} onClick={resetFilters}>Reset filters</button>
                     <button className={styles.updateButton}>Update results</button>
                 </div>
             </div>
