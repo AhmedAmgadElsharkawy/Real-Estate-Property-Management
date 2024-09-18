b926c649bdd4c6727ed26918c5a740d9455bba2f
AbdullahMahmoud2003 <116839669+AbdullahMahmoud2003@users.noreply.github.com>
2024-09-18 20:10:38 +0300

    fixing dashboard properties responsitivity

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -11,7 +11,7 @@ function Account() {
             <Dashboard mine={data.length} favorite={data.length}/>
             <div className={styles.rightDiv}>
                 <img className={styles.accountImg} src={img} alt=""/>
-                <EditProfile name={"Abudllah Mahmoud Hanafy"} email={"abdullah@Gmail.com"} phone={"01148770014"} gender={'M'} city={"Giza"}/>
+                <Properties data={data}/>
             </div>
         </div>
     )
