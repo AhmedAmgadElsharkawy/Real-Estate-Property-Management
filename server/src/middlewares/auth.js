e2b57a641e4093471fb27c3b49be9d7f5eb29dff
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-09-27 02:04:58 +0300

    create auth middleware

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,5 +1,16 @@
+import jwt from "jsonwebtoken"
 
-
-const authenticate = ()=>{
-
+const authenticate = (req,res,next)=>{
+    const token = req.headers.authorization?.split(" ")[1]
+    try {
+        if(!token)
+            return res.status(401).json({ message: 'No token provided' });
+        const decoded = jwt.verify(token,process.env.TOKEN_SECRET);
+        if(!verifyToken)
+            return res.status(401).json({message:"innvalid token"})
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({message:error.message})
+    }
 }
