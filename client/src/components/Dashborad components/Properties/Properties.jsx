c7517528a691bec81f36e14e6decbc496796ab76
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-09-30 20:46:45 +0300

    update account page

--- client/src/components/Dashborad components/Properties/Properties.jsx
+++ client/src/components/Dashborad components/Properties/Properties.jsx
@@ -1,26 +1,12 @@
 /* eslint-disable react/prop-types */
 import styles from './Properties.module.css';
 import PropertyCard from '../../PropertyCard/PropertyCard';
+import {HorizontalSlider} from "../../../components"
 
 function Properties({data}) {
     return (
         <div className={styles.contentDiv}>
-            {
-                data.map((card, index) => {
-                    return (
-                        <PropertyCard
-                            key={index}
-                            id={index}
-                            images={card.images}
-                            type={card.type}
-                            furniture={card.furniture}
-                            location={card.location}
-                            price={card.price}
-                            beds={card.beds}
-                            baths={card.baths}
-                            status={card.status}
-                    />)
-            })}
+           <HorizontalSlider data={data}/>
         </div>
     )
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,27 +4,30 @@ import styles from "./NavBar.module.css";
 import logo from "./logo.jpeg";
 import MenuIcon from '@mui/icons-material/Menu';
 import AuthOverlay from "../AuthOverlay/AuthOverlay";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useAuth } from "../../contexts/AuthContext";
 
 import AddProperty from "../AddProperty/AddProperty";
 function NavBar() {
   const [showAddProperty, setShowAddProperty] = useState(false);
 
-  const [showSignIn,setShowSignIn] = useState(false);
-  const [showSignUp,setShowSignUp] = useState(false);
+  const [showSignIn, setShowSignIn] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const auth = useAuth();
 
 
-  const openSignIn=(e)=>{
+  const openSignIn = (e) => {
     setShowSignIn(true)
     setShowSignUp(false)
   }
 
-  const openSignUp=(e)=>{
+  const openSignUp = (e) => {
     setShowSignUp(true)
     setShowSignIn(false)
   }
 
-  const closeOverlay = (e)=>{
+  const closeOverlay = (e) => {
     setShowSignIn(false)
     setShowSignUp(false)
   }
@@ -41,26 +44,29 @@ function NavBar() {
 
   return (
     <>
-    <div className={styles.container}>
-      <div className={styles.logoDev}>
-        <label htmlFor="">Project Title</label>
-        <img className={styles.img} src={logo} alt="" />
+      <div className={styles.container}>
+        <div className={styles.logoDev}>
+          <label htmlFor="">Project Title</label>
+          <img className={styles.img} src={logo} alt="" />
+        </div>
+        <div className={styles.labelsContainer}>
+          <Link to="/" className={styles.label}>Home</Link>
+          <Link to="/properties" className={styles.label}>Properties</Link>
+          {
+            auth.isAuthenticated ? <Link to="/account" className={styles.label}>account</Link> :
+              <label onClick={openSignIn} className={styles.label}>Sign in</label>
+          }
+        </div>
+        <button onClick={openAddProperty} className={styles.button}>
+          Add Property
+        </button>
+        <div className={styles.menuDev}>
+          <MenuIcon fontSize="large" />
+        </div>
       </div>
-      <div className={styles.labelsContainer}>
-        <Link to = "/" className={styles.label}>Home</Link>
-        <Link to = "/properties" className={styles.label}>Properties</Link>
-        <label onClick = {openSignIn} className={styles.label}>Sign in</label>
-      </div>
-      <button onClick={openAddProperty} className={styles.button}>
-        Add Property
-      </button>
-      <div className={styles.menuDev}>
-        <MenuIcon fontSize="large"/>
-      </div>
-    </div>
-    {showSignIn && <AuthOverlay type = "sign-in" toggleOverflow = {openSignUp} closeOverlay={closeOverlay}/>}
-    {showSignUp && <AuthOverlay type = "sign-up" toggleOverflow = {openSignIn} closeOverlay={closeOverlay}/>}
-    {showAddProperty && <AddProperty onClose={closeAddProperty} />}
+      {showSignIn && <AuthOverlay type="sign-in" toggleOverflow={openSignUp} closeOverlay={closeOverlay} />}
+      {showSignUp && <AuthOverlay type="sign-up" toggleOverflow={openSignIn} closeOverlay={closeOverlay} />}
+      {showAddProperty && <AddProperty onClose={closeAddProperty} />}
     </>
   )
 }
