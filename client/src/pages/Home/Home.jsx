2e25a9d592fface7dfcd7dffef90d6fda5916e79
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2024-11-25 22:32:14 +0200

    add search fucntionality in the home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import SellIcon from '@mui/icons-material/Sell';
 import CloseIcon from '@mui/icons-material/Close';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
-import {HorizontalSlider } from "../../components";
+import { HorizontalSlider } from "../../components";
 import { useState, useEffect } from "react";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import FeatureCard from "../../components/Feature Card/FeatureCard.jsx";
@@ -15,13 +15,15 @@ import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 import UsbOffIcon from '@mui/icons-material/UsbOff';
 import SupportAgentIcon from '@mui/icons-material/SupportAgent';
 import ReviewCard from "../../components/Review Card/ReviewCard.jsx";
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 function Home() {
   const [cityName, setCityName] = useState("");
   const [data, setData] = useState([]);
+  const [fitleredProperties, setFilteredProperties] = useState([])
+  const [displayedPropertiesStatus, setDisplayedPropertiesStatus] = useState("all")
 
   function changeCityName(event) {
     const newCityName = event.target.value;
@@ -32,6 +34,28 @@ function Home() {
     setCityName("");
   }
 
+  function searchOnPropertyByLocation() {
+    var filterResult;
+    if (displayedPropertiesStatus == "all")
+      filterResult = data.filter((property) => { return property.location.toLowerCase().includes(cityName.toLowerCase()) })
+    else
+      filterResult = data.filter((property) => { return property.status.toLowerCase().includes(displayedPropertiesStatus.toLowerCase()) && property.location.toLowerCase().includes(cityName.toLowerCase()) })
+    setFilteredProperties(filterResult)
+  }
+
+  function toggleStatus(newStatus) {
+    var filterResult;
+    if (newStatus == displayedPropertiesStatus) {
+      filterResult = data.filter((property) => { return property.location.toLowerCase().includes(cityName.toLowerCase()) })
+      setDisplayedPropertiesStatus("all");
+    }
+    else {
+      filterResult = data.filter((property) => { return property.status.toLowerCase().includes(newStatus.toLowerCase()) && property.location.toLowerCase().includes(cityName.toLowerCase()) })
+      setDisplayedPropertiesStatus(newStatus);
+    }
+    setFilteredProperties(filterResult)
+  }
+
   useEffect(() => {
     // Fetch data from backend when the component is mounted
     async function getData() {
@@ -39,6 +63,7 @@ function Home() {
         const response = await axios.get("http://localhost:3000/property/get-all-properties");
         // Set the fetched data to state
         setData(response.data);
+        setFilteredProperties(response.data)
       } catch (error) {
         console.log("Error fetching properties:", error);
       }
@@ -53,20 +78,20 @@ function Home() {
       <div className={styles.headerOverlay}>Discover Your Dream Home</div>
       <div className={styles.textOverlay}>Your one-stop real estate destination for buying, renting, and selling properties.</div>
       <div className={styles.searchDev}>
-        <div className={styles.searchBarDev}><div className={styles.removedIcon}><SellIcon fontSize="small" /></div><label className={styles.searchBarLabel} htmlFor="">For Sale</label></div>
-        <div className={styles.searchBarDev}><div className={styles.removedIcon}><BusinessCenterIcon fontSize="small" /></div><label className={styles.searchBarLabel} htmlFor="">For Rent</label></div>
+        <button className={`${styles.searchBarDev} ${displayedPropertiesStatus == "forSale" ? styles.activatedStatus : null}`} onClick={() => { toggleStatus("forSale") }}><div className={styles.removedIcon}><SellIcon fontSize="small" /></div><label className={styles.searchBarLabel} htmlFor="">For Sale</label></button>
+        <button className={`${styles.searchBarDev} ${displayedPropertiesStatus == "forRent" ? styles.activatedStatus : null}`} onClick={() => { toggleStatus("forRent") }}><div className={styles.removedIcon}><BusinessCenterIcon fontSize="small" /></div><label className={styles.searchBarLabel} htmlFor="">For Rent</label></button>
         <div className={styles.inputAndDeleteDiv}>
           <input className={styles.searchInput} type="text" placeholder="Search by city" value={cityName} onChange={changeCityName} />
           <button className={styles.clearInputButton} onClick={deleteCityName}><div className={styles.nonRemovedIcon}><CloseIcon fontSize="small" /></div></button>
         </div>
-        <button className={styles.searchButton}><div className={styles.removedIcon}><SearchIcon /></div>Search</button>
+        <button className={styles.searchButton} onClick={searchOnPropertyByLocation}><div className={styles.removedIcon}><SearchIcon /></div>Search</button>
       </div>
 
       <div className={styles.propertiesWrapper}>
         <div className={styles.properties}>
           <h1>Featured Properties</h1>
-          {data.length > 0 ? <HorizontalSlider data={data} /> : <p>No properties available</p>}
-          
+          {fitleredProperties.length > 0 ? <HorizontalSlider data={fitleredProperties} /> : <p>No properties available</p>}
+
         </div>
       </div>
 
@@ -101,8 +126,8 @@ function Home() {
           <ReviewCard />
         </div>
       </div>
-      
-            
+
+
       <ToastContainer />
     </div>
   )
